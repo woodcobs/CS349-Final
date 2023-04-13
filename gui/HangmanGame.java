1b4a6c5d52389baef94ab2aa21eeb63b8143eb6e
tmandell01 <tmandell01@gmail.com>
2023-04-13 17:06:00 -0400

    Edited

--- gui/HangmanGame.java
+++ gui/HangmanGame.java
@@ -1,16 +1,70 @@
 package gui;
 
 import javax.swing.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
   
 import app.JApplication;
+import io.ResourceFinder;
 
-public class EasyHangmanGame extends JApplication {
+public class HangmanGame extends JApplication {
 	
 	protected JTextField letterField;
 	private JButton aboutButton;
+	private String word;
+	private String gameWord;
+	private List<String> words;
 	
-	public EasyHangmanGame(String[] args, int width, int height) {
+	public HangmanGame(String[] args, int width, int height, String difficulty) {
 		super(args, width, height);
+		
+		ResourceFinder finder = ResourceFinder.createInstance(resources.Marker.class);
+		String filename = difficulty + ".txt";
+		InputStream is = finder.findInputStream(filename);
+		BufferedReader in = new BufferedReader(new InputStreamReader(is));
+		
+		words = new ArrayList<String>();
+		String currWord = "";
+		try
+		{
+			while ((currWord = in.readLine()) != null)
+			{
+				words.add(currWord);
+			}
+		} catch (IOException ioe)
+		{
+			words.add("error");
+		}
+	}
+	
+	public void getWord()
+	{
+		Random rand = new Random();
+		int rand_int = rand.nextInt(60);
+		word = words.get(rand_int);
+		String tempWord = "";
+		// Add spaces
+		
+		gameWord = "";
+		for (int i = 0; i < word.length(); i++)
+		{
+			if (i % 2 == 0)
+			{
+				tempWord += word.charAt(i);
+				gameWord += "_";
+			} else
+			{
+				tempWord += " " + word.charAt(i);
+				gameWord += " _";
+			}
+		}
+		word = tempWord;
 		
 	}
 
@@ -26,6 +80,8 @@ public class EasyHangmanGame extends JApplication {
 		letterField = new JTextField();
 		letterField.setBounds(80, 30, 200, 30);
 		contentPane.add(letterField);
+		
+		
 		
 	}
 	
