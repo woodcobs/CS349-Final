62531b8cb5601aecb996c188e61359d72a1c0cdf
Bradley Woodcock <BradWoodcock00@gmail.com>
2023-04-13 13:02:52 -0400

    Added Resources

---
+++ resources/Marker.java
@@ -0,0 +1,18 @@
+package resources;
+
+/**
+ * The Marker Class.
+ * @author Bradley Woodcock, Thomas Mandel, Hunter Bowles.
+ */
+public class Marker 
+{
+
+	/**
+	 * The Marker Constructor.
+	 */
+	public Marker() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
